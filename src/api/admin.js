69416e5ba1e390ec4d8dd7110b38d55c9efa2346
Yang Xiaoyan <evangelin-lun@foxmail.com>
2021-08-19 14:51:18 +0800

    Change the method of a function

--- src/api/admin.js
+++ src/api/admin.js
@@ -25,66 +25,9 @@ export default{
     delWorker(userId,workerId){
         return request({
             url: `/admin/delworker/${userId}`,
-            method: 'post',
+            method: 'delete',
             data: workerId       
         })
     },
 
-
-
-
-
-    getVList(chosenPlatform){
-        return request({
-            url: `/task/getvlist/${chosenPlatform}`,
-            method: 'get'
-        })
-    },
-    
-    getPlatformList(){
-        return request({
-            url: `/task/getplatform`,//ES6写法
-            method: 'get'
-        });
-    },
-    
-    createTaskUrl(pojo){
-        return request({
-            url: `/task/createurl`,
-            method: 'post',
-            data: pojo
-        })
-    },
-
-    saveTaskInfo(pojo){
-        return request({
-            url: `/task/savetaskinfo`,
-            method: 'post',
-            data: pojo       
-        })
-    },
-    
-
-
-    getTasksId(){
-        return request({
-            url: `/task/getidlist`,//ES6写法
-            method: 'get'
-        });
-    },
-
-    taskProfile(pojo){
-        return request({
-            url: `/task/taskprofile`,//ES6写法
-            method: 'post',
-            data: pojo
-        });
-    },
-
-    netVisualize(taskId){
-        return request({
-            url: `/task/netvisualize/${taskId}`,//ES6写法
-            method: 'get'
-        });
-    },
 } 
